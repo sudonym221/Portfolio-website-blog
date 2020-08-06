aa4ff217a3e15d967e14485072d2ca8ea44ec9f9
[disglair] <[deepjchetia007@gmail.com]>
2020-08-06 22:44:27 +0530

    auth var added for admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const adminController = require('../controllers/adminControllers.js');
+const {auth} = require('../models/admin.js');
 
-router.get('/adminReg', adminController.admin_reg);
+router.get('/adminReg', auth, adminController.admin_reg);
 
-router.post('/newAdmin', adminController.admin_save)
+router.post('/newAdmin', auth, adminController.admin_save)
 
 router.get('/admin', adminController.get_admin_page);
 
